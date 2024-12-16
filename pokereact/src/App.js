3f5fb647c8ccdcf2186804bd1fa532da8b275bce
Rodrigo Santos <jsantosn23@fpcoruna.afundacion.org>
2024-12-16 10:53:13 +0100

    Pokemon y Pokelista añadidos con useEffect()

--- pokereact/src/App.js
+++ pokereact/src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Pokemon from './components/Pokemon';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import PokeLista from './components/Pokelista';
 
 function App() {
   return (
-    <Pokemon />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PokeLista />} />
+        <Route path="/pokemon/:id" element={<Pokemon />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
