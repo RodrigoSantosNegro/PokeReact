f63e5873c95c19690297fc7bf4f73f321dc5d5d7
Rodrigo Santos <jsantosn23@fpcoruna.afundacion.org>
2025-01-08 11:00:59 +0100

    Link y lista pokimons

--- pokereact/src/components/Pokelista.js
+++ pokereact/src/components/Pokelista.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 
 const PokeLista = (props) => {
     const [pokemons, setPokemons] = useState([]);
+    const navigate = useNavigate();
 
     useEffect(() => {
         axios.get("https://pokeapi.co/api/v2/pokemon").then(response => {
@@ -10,10 +11,19 @@ const PokeLista = (props) => {
         })
     }, [])
 
+    const funcionNavegarASnivy = () => {
+        navigate("/pokemon/495");
+    }
 
     return <div>
+        <Link to="/pokemon/25">Ir a Pikachu</Link>
         <h1>Lista</h1>
-        {pokemons.map(p => <p>Bo nadal</p>)}
+        {pokemons.map(p => {
+            <div>
+                <p onClick={() => {navigate(`/pokemon/${p.name}`)}}>¿Cuál es este pokemon? Es... {p.name}</p>
+                <Link to={"/pokemon/"+p.name}>Navegar</Link>
+            </div>
+        })}
     </div>
 }
 
